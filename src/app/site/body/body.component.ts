8b26a3a69a2bd562df3c933226cacc02fab1aee8
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-01-31 20:33:35 +0100

    Add logout button

--- src/app/site/body/body.component.ts
+++ src/app/site/body/body.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { combineLatest, Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import * as AuthActions from '../../auth/store/auth.actions';
 import * as fromApp from '../../store/app.reducer';
 
 
@@ -39,6 +40,10 @@ export class BodyComponent implements OnInit {
 
   ngOnInit() {
 
+  }
+
+  onLogout() {
+    this.store.dispatch(new AuthActions.Logout());
   }
 
 }
