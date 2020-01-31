b5706ef024861e4934724d64de343d4bf85d8a6f
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-01-31 14:14:46 +0100

    Basic setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { LayoutModule } from '@angular/cdk/layout';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -13,11 +17,15 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { BodyComponent } from './site/body/body.component';
 import { ContentComponent } from './site/body/content/content.component';
 import { ProjectComponent } from './site/body/project/project.component';
 import { HeaderComponent } from './site/header/header.component';
 import * as fromApp from './store/app.reducer';
+
+
 
 @NgModule({
   declarations: [
@@ -26,6 +34,8 @@ import * as fromApp from './store/app.reducer';
     BodyComponent,
     ContentComponent,
     ProjectComponent,
+    AuthComponent,
+    LoadingSpinnerComponent,
   ],
   imports: [
     BrowserModule,
@@ -40,6 +50,10 @@ import * as fromApp from './store/app.reducer';
     StoreModule.forRoot(fromApp.appReducer),
     StoreDevtoolsModule.instrument({ logOnly: environment.production }),
     MatTreeModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kosaml-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.scss']
+})
+export class AuthComponent implements OnInit {
+  isLoginMode = true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+}
