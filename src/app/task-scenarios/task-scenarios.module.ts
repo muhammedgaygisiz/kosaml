20d6795f697cb896e754f13f8f0fabb93ed41860
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-02-09 21:26:06 +0100

    Add task scenarios module

---
+++ src/app/task-scenarios/task-scenarios.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TaskScenariosRoutingModule } from './task-scenarios-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TaskScenariosRoutingModule
+  ]
+})
+export class TaskScenariosModule { }
