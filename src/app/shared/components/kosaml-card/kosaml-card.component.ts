c22ce3e476d0fb19bfbacf282af8c99b604f6cde
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-02-23 17:11:12 +0100

    Readd quotemark to tslint otherwise it conflicts with prettier and some formatting

--- src/app/shared/components/kosaml-card/kosaml-card.component.ts
+++ src/app/shared/components/kosaml-card/kosaml-card.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "kosaml-card",
-  templateUrl: "./kosaml-card.component.html",
-  styleUrls: ["./kosaml-card.component.scss"]
+  selector: 'kosaml-card',
+  templateUrl: './kosaml-card.component.html',
+  styleUrls: ['./kosaml-card.component.scss']
 })
 export class KosamlCardComponent implements OnInit {
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() { }
+  ngOnInit() {}
 }
