399b84c58b2219b55cee4d7d71c8c9ef62c4c5ff
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-02-27 19:44:25 +0100

    Implement resize behavior of sidebar

--- src/app/site/components/sidebar/sidebar.component.ts
+++ src/app/site/components/sidebar/sidebar.component.ts
@@ -25,13 +25,58 @@ export class SidebarComponent {
   @ViewChild("sidenav", { static: false, read: ElementRef })
   matSideNav: ElementRef;
 
-  startingXPosition: number;
+  xOffset: number;
+  startingWidth: number;
   isResizing: boolean = false;
-  mouseLastSeen: number = -1;
 
   constructor() { }
 
   onLogout() {
     this.logout.emit()
+  }
+
+  onMouseDown(event) {
+    this.isResizing = true;
+
+    // Setup
+    this.xOffset = event.pageX;
+    this.startingWidth = this.matSideNav.nativeElement.clientWidth;
+
+    this.setMouseCursorToResize();
+
+    event.preventDefault();
+  }
+
+  onMouseUp() {
+    this.isResizing = false;
+    this.resetMouseCursor();
+  }
+
+  onMouseMove(event) {
+    event.preventDefault();
+
+    if (this.isResizing) {
+      this.setNewWidth(
+        event.pageX - this.xOffset + this.startingWidth
+      )
+    }
+  }
+
+  private resetMouseCursor() {
+    document.documentElement.style.cursor = "default"
+  }
+
+  private setMouseCursorToResize() {
+    document.documentElement.style.cursor = "col-resize";
+  }
+
+  private calculateNewWidth(event) {
+    return this.matSideNav.nativeElement.scrollWidth
+      + (event.screenX - this.xOffset)
+  }
+
+  private setNewWidth(newWidth: number) {
+    this.matSideNav.nativeElement.style.width =
+      `${newWidth}px`;
   }
 }
