61b92a7c7b499eef4b88dc08bbecf9477be821a1
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-03-03 20:21:23 +0100

    Move menu bar to auth section

--- src/app/site/containers/app.component.ts
+++ src/app/site/containers/app.component.ts
@@ -15,7 +15,6 @@ import { FileNode } from '../models';
       (toggleProjectBar)="onToggleProjectBar()"
       (toggleToolBar)="onToggleToolBar()"
     ></kosaml-header>
-    <kosaml-menu-bar></kosaml-menu-bar>
     <kosaml-body
       [isAuthenticated]="isAuthenticated$ | async"
       [isProjectBarOpen]="isProjectBarOpen$ | async"
