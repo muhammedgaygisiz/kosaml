5ca0b897efbe9dee33deb743d852ccee78fe6ebf
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-04-12 16:08:52 +0200

    Moved Logout from sidebar to menubar

--- src/app/site/components/menu-bar/menu-bar.component.ts
+++ src/app/site/components/menu-bar/menu-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'kosaml-menu-bar',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-bar.component.scss']
 })
 export class MenuBarComponent {
+  @Output()
+  logout = new EventEmitter();
 
+  onLogout() {
+    this.logout.emit()
+  }
 }
