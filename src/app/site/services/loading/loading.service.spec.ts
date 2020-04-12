fa6a700cef489e4c59a6d77c2f21d7b43b96d924
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-04-12 18:01:47 +0200

    Fix tests

--- src/app/site/services/loading/loading.service.spec.ts
+++ src/app/site/services/loading/loading.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
+import { StoreModule } from '@ngrx/store';
 import { LoadingService } from './loading.service';
 
 
 describe('LoadingServiceService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      StoreModule.forRoot({}),
+    ]
+  }));
 
   it('should be created', () => {
     const service: LoadingService = TestBed.get(LoadingService);
