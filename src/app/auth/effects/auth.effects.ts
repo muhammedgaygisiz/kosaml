d487ffa8c2085a372fad2877419b67b9a621afd6
Muhammed Gaygisiz <54054345+muhammedgaygisiz@users.noreply.github.com>
2020-04-12 18:23:04 +0200

    Switch to createEffect (#52)

--- src/app/auth/effects/auth.effects.ts
+++ src/app/auth/effects/auth.effects.ts
@@ -1,12 +1,10 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
-import { Actions, createEffect, Effect, ofType } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { from, of } from 'rxjs';
-import { catchError, map, switchMap, tap } from 'rxjs/operators';
+import { map, switchMap, tap } from 'rxjs/operators';
 import { LoadingService } from 'src/app/site/services';
-import { environment } from '../../../environments/environment';
 import { AuthActions } from '../actions';
 
 
@@ -19,33 +17,10 @@ export interface AuthResponseData {
   registered?: boolean;
 }
 
-const handleError = errorRes => {
-  let errorMessage = 'An unknown error occured!';
-
-  if (!errorRes.error || !errorRes.error.error) {
-    return of(AuthActions.authenticationFailed({ error: errorMessage }));
-  }
-
-  switch (errorRes.error.error.message) {
-    case 'EMAIL_EXISTS':
-      errorMessage = 'This email exists already!';
-      break;
-    case 'EMAIL_NOT_FOUND':
-      errorMessage = 'This email does not exist.';
-      break;
-    case 'INVALID_PASSWORD':
-      errorMessage = 'Could not log in.';
-      break;
-  }
-
-  return of(AuthActions.authenticationFailed({ error: errorMessage }));
-};
-
 @Injectable()
 export class AuthEffects {
   constructor(
     private actions$: Actions,
-    private http: HttpClient,
     private router: Router,
     private firebaseAuth: AngularFireAuth,
     private loadingService: LoadingService
@@ -63,29 +38,17 @@ export class AuthEffects {
     );
   });
 
-  @Effect()
-  authSignup = this.actions$.pipe(
-    ofType(AuthActions.startSignUp),
-    switchMap(signupAction => {
-      return this.http
-        .post<AuthResponseData>(
-          `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebaseAPIKey}`,
-          {
-            email: signupAction.email,
-            password: signupAction.password,
-            returnSecureToken: true,
-          },
-        )
-        .pipe(
-          tap(resDate => {
-          }),
-
-          catchError(errorRes => {
-            return handleError(errorRes);
-          }),
-        );
-    }),
-  );
+  authSignup$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(AuthActions.startSignUp),
+      tap(() => this.loadingService.startLoading()),
+      switchMap(authData => from(this.firebaseAuth.auth.createUserWithEmailAndPassword(
+        authData.email,
+        authData.password,
+      ))),
+      map(() => AuthActions.authenticationSucceeded({ redirect: true })),
+    );
+  });
 
   redirectAfterLogin$ = createEffect(() => {
     return this.actions$.pipe(
